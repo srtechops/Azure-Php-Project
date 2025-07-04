2b5575f44498755160d9208f3ebc31a563ce556e
shammugaraja M <shammugarajaofficial@gmail.com>
2025-07-04 07:44:48 +0530

    v1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DevOps Training</title>
+</head>
+<body style="background-color:#f0f8ff; font-family: Arial, sans-serif;">
+
+<?php
+echo '<h1 style="color:#2e8b57; text-align:center; margin-top: 100px;">
+        Welcome to <span style="color:#ff4500;">SRTECHOPS DevOps Training V2</span>!
+      </h1>';
+?>
+
+</body>
+</html>
